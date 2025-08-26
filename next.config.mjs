17c7c8bbe631219ab27d549f94bd76c9c422b903
Oleksandr Rapin <oleksandr.rpn@gmail.com>
2025-08-26 21:00:39 +0300

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
-import withExportImages from 'next-export-optimize-images';
+import withExportImages from "next-export-optimize-images";
+
+const repoName = "auto-service";
 
 const nextConfig = withExportImages({
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
   compiler: {
     emotion: true,
   },
