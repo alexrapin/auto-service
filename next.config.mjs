ea06a77613ef5df84557636b6ab02eac7af21d7e
Oleksandr Rapin <oleksandr.rpn@gmail.com>
2025-08-26 21:12:08 +0300

    add

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 import withExportImages from "next-export-optimize-images";
 
-const repoName = "auto-service";
+const repoName = "auto-service"; // ⚡️ имя твоего репозитория на GitHub
 
 const nextConfig = withExportImages({
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
+  output: "export", // для GitHub Pages
   basePath: `/${repoName}`,
   assetPrefix: `/${repoName}/`,
   compiler: {
