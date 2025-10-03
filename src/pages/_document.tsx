cf25a040e69722698dd9fd63f70b1dd536bc035f
alexrapin <or@offer-wall.com>
2025-10-03 13:29:45 +0300

    optimize

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // pages/_document.tsx
-import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
 import createEmotionServer from "@emotion/server/create-instance";
 import createCache from "@emotion/cache";
 
@@ -46,12 +52,30 @@ export default class MyDocument extends Document {
         <Head>
           {/* Preconnect to Google Fonts to speed up font fetch */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-          {/* Load Inter (and Roboto fallback) from Google Fonts */}
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Roboto:wght@400;700&display=swap"
-            rel="stylesheet"
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin=""
           />
+
+          {/* Non-blocking load: preload stylesheet then switch rel to stylesheet on load */}
+          <link
+            rel="preload"
+            as="style"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Roboto:wght@400;700&display=swap"
+            id="gf-preload"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(() => { const l = document.getElementById('gf-preload'); if (l) { l.addEventListener('load', function() { (this as HTMLLinkElement).rel = 'stylesheet'; }); } })()`,
+            }}
+          />
+          <noscript>
+            <link
+              href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Roboto:wght@400;700&display=swap"
+              rel="stylesheet"
+            />
+          </noscript>
         </Head>
         <body>
           <Main />
